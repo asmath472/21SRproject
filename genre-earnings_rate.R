2533e248f003b7275d2f14b1a7069cc4897bd503
jaewan01 <83945246+jaewan01@users.noreply.github.com>
2021-06-04 01:00:55 +0900

    Add files via upload

--- genre-earnings_rate.R
+++ genre-earnings_rate.R
@@ -4,9 +4,9 @@ library(stringr)
 movie_metadata <- read.csv("movies_metadata.csv", header = T)
 tibble_metadata <- as_tibble(movie_metadata)
 movie_data_genre <- tibble_metadata %>%
-                      filter(!is.na(genres), !is.na(budget), !is.na(revenue)) %>%
-                      filter(budget > 0, revenue > 0) %>%
-                      select(id, genres, budget, revenue)
+  filter(!is.na(genres), !is.na(budget), !is.na(revenue)) %>%
+  filter(budget > 0, revenue > 0) %>%
+  select(id, genres, budget, revenue)
 movie_data_genre$budget <- as.numeric(movie_data_genre$budget)
 movie_data_genre$revenue <- as.numeric(movie_data_genre$revenue)
 movie_data_genre <- mutate(movie_data_genre, earnings_rate = revenue/budget)
@@ -14,8 +14,10 @@ d <- dim(movie_data_genre)
 for (i in 1:d[1]) {
   movie_data_genre[i, ]$genres <- gsub(",|\\[|\\]|\\{","",movie_data_genre[i, ]$genres)
   movie_data_genre[i, ]$genres <- gsub(" ","",movie_data_genre[i, ]$genres)
+  if (grepl("jpg", movie_data_genre[i, ]$genres))
+    movie_data_genre[i, ]$genres <- NA
 }
-movie_data_genre <- movie_data_genre %>% separate(genres, into = c("genre1", "genre2", "genre3", "genre4", "genre5", "genre6", "genre7", "genre8"), sep = "}")
+movie_data_genre <- movie_data_genre %>% filter(!is.na(genres))%>%separate(genres, into = c("genre1", "genre2", "genre3", "genre4", "genre5", "genre6", "genre7", "genre8"), sep = "}")
 movie_data_genre
 new_df <- data.frame(id = character(), genre = character(), earnings_rate = double())
 str(new_df)
@@ -51,6 +53,6 @@ for (i in 1:d[1]) {
 }
 seperated_by_genre <- as_tibble(new_df)
 result <- seperated_by_genre %>%
-          group_by(genre) %>%
-          summarise(mean_earning = mean(earnings_rate))
+  group_by(genre) %>%
+  summarise(mean_earning = mean(earnings_rate))
 ggplot(data = result, aes(x = genre, y = mean_earning)) + geom_col()
